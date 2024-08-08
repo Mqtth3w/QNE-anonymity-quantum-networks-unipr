4acc19425fa7e06d5f68d7923da5cf38615a997a
Mqtth3w <mqtth3w@duck.com>
2024-08-08 13:28:04 +0200

    Add files via upload

--- E91/src/app_alice.py
+++ E91/src/app_alice.py
@@ -1,6 +1,29 @@
+from netqasm.sdk import EPRSocket
+from netqasm.sdk.external import NetQASMConnection
+
 def main(app_config=None, x=0, y=0):
     # Put your code here
-    return {}
+    
+    # Specify an EPR socket to bob
+    epr_socket = EPRSocket("bob")
+
+    alice = NetQASMConnection(
+        app_name=app_confing.app_name, #"alice",
+        log_config=app_config.log_config,
+        epr_sockets=[epr_socket],
+    )
+    with alice:
+        # Create an entangled pair using the EPR socket to bob
+        q_ent = epr_socket.create()[0] # .create_keep() maintain it in memory, .create() don't
+        # Measure the qubit
+        alice.flush() #ensure previous commands are executed, it is done automatically when you go out of the scope
+        m = q_ent.measure()
+    
+    m = int(m)
+    # Print the outcome
+    print(f"alice's outcome is: {m}")
+    
+    return {"Alice": m}
 
 
 if __name__ == "__main__": 
--- E91/src/app_bob.py
+++ E91/src/app_bob.py
@@ -1,6 +1,29 @@
+from netqasm.sdk import EPRSocket
+from netqasm.sdk.external import NetQASMConnection
+
 def main(app_config=None, x=0, y=0):
     # Put your code here
-    return {}
+    
+    # Specify an EPR socket to alice
+    epr_socket = EPRSocket("alice")
+
+    bob = NetQASMConnection(
+        app_name=app_confing.appname, #"bob", 
+        log_config=app_config.log_config,
+        epr_sockets=[epr_socket],
+    )
+    with bob:
+        # Receive an entangled pair using the EPR socket to alice
+        q_ent = epr_socket.recv()[0] # .recv_keep() maintain it in memory, .recv() don't
+        # Measure the qubit
+        bob.flush() #ensure previous commands are executed, it is done automatically when you go out of the scope
+        m = q_ent.measure()
+        
+    m = int(m)
+    # Print the outcome
+    print(f"bob's outcome is: {m}")
+    
+    return {"Bob": m}
 
 
 if __name__ == "__main__": 
