55700129807ea8fc3c24e8bb64bc553af57677fa
Mqtth3w <mqtth3w@duck.com>
2024-08-10 19:32:19 +0200

    Add files via upload

---
+++ anonymous-communication/src/protocols.py
@@ -0,0 +1,28 @@
+"""
+@author Matteo Gianvenuti https://github.com/mqtth3w
+@license GPL-3.0
+"""
+
+def protocol_RandomBit():
+    pass
+
+
+def protocol_Parity():
+    pass
+
+
+def protocol_Notification():
+    pass
+
+
+def protocol_Anonymous_Entanglement():
+    pass
+
+
+def protocol_Verification():
+    pass
+
+
+def protocol_LogicalOR():
+    pass
+
